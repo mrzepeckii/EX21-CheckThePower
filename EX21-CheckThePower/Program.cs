b33eae90ad43461368898d02361f9e7ef1c13f4b
mrzepecki <m.rzepaa@gmail.com>
2020-07-07 16:34:54 +0200

    Add project files.

---
+++ EX21-CheckThePower/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.IO;
+
+namespace EX21_CheckThePower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            int[] squares = { 1, 4, 9, 16, 25 };
+            Console.WriteLine(checkThePower( numbers, squares));
+        }
+
+
+        static bool checkThePower(IEnumerable<int> numbers, IEnumerable<int> squares)
+        {
+            var numberToPower = from number in numbers
+                                select number * number;
+            if (numberToPower.SequenceEqual(squares))
+                return true;
+            return false;
+        }
+    }
+}
